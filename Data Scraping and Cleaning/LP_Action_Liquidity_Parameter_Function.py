271536049ced26835b73c36dfbeca136c52addda
zhiconghu <zhiconghu@gmail.com>
2023-07-04 19:31:01 +0100

    nft_id

--- Data Scraping and Cleaning/LP_Action_Liquidity_Parameter_Function.py
+++ Data Scraping and Cleaning/LP_Action_Liquidity_Parameter_Function.py
@@ -1,3 +1,4 @@
+import numpy as np
 from sympy import symbols, Symbol, Eq, nsolve, solve, sqrt
 from decimal import Decimal, getcontext
 
@@ -14,6 +15,6 @@ def Get_Liquidity_Parameter(row_tuple):
 
     L = Symbol('L')
     L = solve((passed_row[0] + L/(passed_row[3].sqrt())) * (passed_row[1] + L*(passed_row[2].sqrt())) - L**2, L, prec = 10)
-    L = L[1]
+    L = [x for x in L if x > 0][0]
 
     return L
