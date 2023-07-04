0b981f79fee03568230e4b3fd7aa9e6621d1f115
zhiconghu <zhiconghu@gmail.com>
2023-07-04 16:53:07 +0100

    Liquidity Parameter

---
+++ Data Scraping and Cleaning/LP_Action_Liquidity_Parameter_Function.py
@@ -0,0 +1,19 @@
+from sympy import symbols, Symbol, Eq, nsolve, solve, sqrt
+from decimal import Decimal, getcontext
+
+
+def Get_Price(_tick_):
+    return Decimal(1/(1.0001**_tick_*10**-12))
+
+
+def Get_Liquidity_Parameter(row_tuple):
+
+    if row_tuple[0]%10000 == 0: print("Working on row:", row_tuple[0])
+
+    passed_row = row_tuple[1]
+
+    L = Symbol('L')
+    L = solve((passed_row[0] + L/(passed_row[3].sqrt())) * (passed_row[1] + L*(passed_row[2].sqrt())) - L**2, L, prec = 10)
+    L = L[1]
+
+    return L
